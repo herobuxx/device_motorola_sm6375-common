7ced9a3bf386144093baa8672843301520097557
HeroBuxx <me@herobuxx.me>
2026-02-21 09:29:52 +0000

    sm6375-common: configs: Commonize powerhint
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- common.mk
+++ common.mk
@@ -282,6 +282,10 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
+    $(LOCAL_PATH)/configs/power/powerhint_sm6375ac.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint_sm6375ac.json
+
 # Properties
 PRODUCT_VENDOR_PROPERTY_BLACKLIST := \
     ro.carrier
